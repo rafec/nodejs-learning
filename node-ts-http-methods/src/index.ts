3121d023b4c01f00baf62364d1e8e0b0589b4562
rafec <rafael.brizuena@gmail.com>
2023-08-28 20:37:46 -0400

    feat: setting up initial routes

--- node-ts-http-methods/src/index.ts
+++ node-ts-http-methods/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import express, { Request, Response } from "express";
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (request: Request, response: Response) => {
+  return response.status(200).json({ message: "DioBank API" });
+});
+
+server.post("/user", (request: Request, response: Response) => {
+  const body = request.body;
+  console.log(body);
+  return response.status(201).json({ message: "User created!" });
+});
+
+server.listen(5000, () => console.log("Server started"));
