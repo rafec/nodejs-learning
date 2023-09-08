94014de907a084e36268982bdfe176a2ebbc8f00
rafec <rafael.brizuena@gmail.com>
2023-09-08 19:41:30 -0400

    feat: creating mocks

--- node-ts-unit-tests/src/controllers/UserController.spec.ts
+++ node-ts-unit-tests/src/controllers/UserController.spec.ts
@@ -1,30 +1,26 @@
-import { Request, Response } from "express";
 import { UserService } from "../services/UserService";
 import { UserController } from "./UserController";
-import { Params } from "express-serve-static-core";
+import { Request } from "express";
+import { makeMockRequest } from "../__mocks__/mockRequest.mock";
+import { MakeMockResponse } from "../__mocks__/mockResponse.mock";
 
 describe("UserController", () => {
-  const mockUserService: Partial<UserService> = {};
+  const mockUserService: Partial<UserService> = {
+    createUser: jest.fn(),
+  };
 
   const userController = new UserController(mockUserService as UserService);
 
-  const makeMockRequest = ({
-    params,
-    query,
-  }: {
-    params?: Params;
-    query?: Params;
-  }): Request => {
-    const request = {
-      params: params || {},
-      query: query || {},
-    } as unknown;
-
-    return request as Request;
-  };
-
   it("Should add a new user.", () => {
-    const mockRequest = makeMockRequest({});
-    const response = userController.createUser(mockRequest);
+    const mockRequest = {
+      body: {
+        name: "Rafael",
+        email: "rafael@mail.com",
+      },
+    } as Request;
+    const mockResponse = MakeMockResponse();
+    userController.createUser(mockRequest, mockResponse);
+    expect(mockResponse.state.status).toBe(201);
+    expect(mockResponse.state.json).toMatchObject({ message: "User created!" });
   });
 });
