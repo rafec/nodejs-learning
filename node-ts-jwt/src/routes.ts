3bc46a2b3bca65716d44dd98288f2bead32daa9a
rafec <rafael.brizuena@gmail.com>
2023-09-20 19:57:11 -0400

    feat: protecting route with middleware

--- node-ts-jwt/src/routes.ts
+++ node-ts-jwt/src/routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import { LoginController } from "./controllers/LoginController";
+import { verifyAuth } from "./middleware/verifyAuth";
 
 export const router = Router();
 
 const userController = new UserController();
 const loginController = new LoginController();
 
-router.get("/user/:userId", userController.getUser);
+router.get("/user/:userId", verifyAuth, userController.getUser);
 router.post("/user", userController.createUser);
 router.delete("/user", userController.deleteUser);
 router.post("/login", loginController.login);
